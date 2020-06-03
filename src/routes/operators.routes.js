9eee98dd864818bc974b27e1e713fea85660e01e
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2020-06-02 21:39:35 -0300

    :bulb: Adicionando documentação (apidoc) nas rotas

--- src/routes/operators.routes.js
+++ src/routes/operators.routes.js
@@ -4,6 +4,19 @@ const OperatorController = require('../controllers/OperatorController');
 
 const routes = Router();
 
+/**
+* @api {get} /status Singin
+* @apiGroup Sistema
+*
+* @apiSuccess {String} status Mensagem de acesso autorizado
+* 
+* @apiSuccessExample {json} Sucesso
+*    HTTP/1.1 200 OK
+*    {
+*      "status": "Serviço está disponível!"
+*    }
+*
+*/
 routes.get('/status',OperatorController.statusApi);
 routes.post('/pay/:operator',OperatorController.pagCartao);
 
